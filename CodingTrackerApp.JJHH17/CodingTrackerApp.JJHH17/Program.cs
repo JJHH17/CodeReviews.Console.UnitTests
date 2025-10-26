dd80ef10a96573717070f189d75ecd7e2b3f596f
James Hatfield <james.hatfield17@icloud.com>
2025-10-26 10:23:07 +0000

    Added reference for creating database file and launching UI

---
+++ CodingTrackerApp.JJHH17/CodingTrackerApp.JJHH17/Program.cs
@@ -0,0 +1,13 @@
+﻿using Menu = CodingTrackerApp.JJHH17.UserInterface;
+using Database = CodingTrackerApp.JJHH17.Database;
+
+namespace CodingTrackerApp.JJHH17;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Database.Database.CreateDatabase();
+        Menu.Running();
+    }
+}
