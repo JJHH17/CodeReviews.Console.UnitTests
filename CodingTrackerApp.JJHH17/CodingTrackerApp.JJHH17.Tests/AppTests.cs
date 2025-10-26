eb4c759ffd2a398d86cb473a7469d14821667573
James Hatfield <james.hatfield17@icloud.com>
2025-10-26 21:14:13 +0000

    Added zero Duration check test

--- CodingTrackerApp.JJHH17/CodingTrackerApp.JJHH17.Tests/AppTests.cs
+++ CodingTrackerApp.JJHH17/CodingTrackerApp.JJHH17.Tests/AppTests.cs
@@ -26,4 +26,13 @@ public class AppTests
 
         Assert.AreNotEqual("0 years, 0 months, 0 days, 3 hours, 0 minutes, 0 seconds", session.GetDuration());
     }
+
+    [TestMethod]
+    public void Duration_ZeroDuration_PassesIfTestPasses()
+    {
+        var start = "2024-01-01 10:00:00";
+        var end = "2024-01-01 10:00:00";
+        var session = new CodingTrackerApp.JJHH17.Models.CodingSession(start, end);
+        Assert.AreEqual("0 years, 0 months, 0 days, 0 hours, 0 minutes, 0 seconds", session.GetDuration());
+    }
 }
